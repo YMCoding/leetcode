4832a7bbed4203d7585d7fdc1527d5413acf748c
yang.xueyi <yang.xueyi@immomo.com>
2020-09-28 10:34:14 +0800

    setprice

---
+++ src/main/java/info/ymcode/study/BinaryTreeNode.java
@@ -0,0 +1,11 @@
+package info.ymcode.study;
+
+public class BinaryTreeNode {
+
+    public int val;
+
+    public BinaryTreeNode right;
+
+    public BinaryTreeNode left;
+
+}
---
+++ src/main/java/info/ymcode/study/search/BinSearch.java
@@ -0,0 +1,8 @@
+package info.ymcode.study.search;
+
+/**
+ * 有序数组2分查找某个数
+ */
+public class BinSearch {
+
+}
